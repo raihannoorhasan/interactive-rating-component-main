d36156c9aede00142deb7d847be74e545cb36d1d
Raihan Noor Hasan <raihanc39@gamil.com>
2022-09-04 23:12:16 +0600

    added javscript for interactive

---
+++ script.js
@@ -0,0 +1,30 @@
+const submitBtn = document.querySelector('.submit-btn')
+const ratingState = document.querySelector('.rating-state')
+const thankYouState = document.querySelector('.thank-you-state')
+const ratingRate = document.querySelector('.rating-rates')
+const ratingNumberGiven = document.querySelector('.rating-number-given')
+const ratingRates = document.querySelectorAll('.rating-rate')
+const ratingIconContainer = document.querySelector('.rating-icon-container')
+const RATING_ICON = `<img src="./images/icon-star.svg" alt="" class="rating-icon" />`
+
+
+ratingRate.addEventListener('click', e => {
+    if (e.target.matches('.rating-rates')) return
+
+    const ratingNumber = e.target.innerText
+    ratingNumberGiven.innerText = ratingNumber;
+
+    ratingIconContainer.innerHTML = ''
+    for (let i = 0; i < ratingNumber; i++) {
+        ratingIconContainer.insertAdjacentHTML('beforeend', RATING_ICON)
+    }
+
+    ratingRates.forEach(r => r.classList.remove('active'))
+    ratingRates[ratingNumber - 1].classList.add('active')
+})
+
+submitBtn.addEventListener('click', () => {
+    ratingState.classList.add('hidden')
+    thankYouState.classList.remove('hidden')
+})
+
